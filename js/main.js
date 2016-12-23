1555666b725d2802a4f98ab2763a3dc05eecc594
Justin Schellenberg <jrschellenberg@Gmail.com>
2016-12-23 12:01:53 -0600

    Added Search functionality with pagination

--- js/main.js
+++ js/main.js
@@ -4,109 +4,120 @@
  * Code used implementing plugin from: https://github.com/bilalakil/bin/blob/master/simplepagination/page-fragment/index.html
  *
  */
+//Making sure all dom elements loaded b4 working on them.
 jQuery(function ($) {
-    //
-    //
-    //
-    //var paginate = function(){
-    //
-    //
-    //    
-    //
-    //
-    //
-    //
-    //
-    //}
-
     var $pageParts = $(".student-item");
-
-    //Making variable of how many items we have.
-    var numPages = $pageParts.length;
-
-    //Setting variable of how many items we require per page
-    var itemsPerPage = 10;
-
-    //Hiding rest of items except first 10.
-    $pageParts.slice(itemsPerPage).hide();
-    var numberOfPaginations = parseInt(numPages / itemsPerPage + 1);
-    console.log(numberOfPaginations);
+    console.log($pageParts);
 
 
-    $(".paginationHolder").pagination({
-        items: numPages,
-        itemsOnPage: itemsPerPage,
-        cssStyle: 'light-theme',
-        displayedPages: 4,
-        onPageClick: function (pageNum) {
-            //determining which page parts to show.
-            var start = itemsPerPage * (pageNum - 1);
-            var end = start + itemsPerPage;
 
-            //Hide all page parts
-            //Then show the ones we require
-            $pageParts.hide().slice(start, end).show();
+    var paginate = function(pageParts){
+        console.log(pageParts);
+        var itemsPerPage = 10;
+        //Setting variable of how many items we require per page
 
+        //Making variable of how many items we have.
+        var numPages = pageParts.length;
+        if(pageParts.length == 0){
+            $('#noSearchResults').show();
+            console.log("got in here??");
+        }
+        else {
+            $('#noSearchResults').hide();
+        }
+
+
+
+        //Hiding rest of items except first 10.
+        pageParts.slice(itemsPerPage).hide();
+        var numberOfPaginations = parseInt(numPages / itemsPerPage + 1);
+        console.log(numberOfPaginations + " is number of pagination");
+
+
+        $(".paginationHolder").pagination({
+            items: numPages,
+            itemsOnPage: itemsPerPage,
+            cssStyle: 'light-theme',
+            displayedPages: 4,
+            onPageClick: function (pageNum) {
+                //determining which page parts to show.
+                var start = itemsPerPage * (pageNum - 1);
+                var end = start + itemsPerPage;
+
+                //Hide all page parts
+                //Then show the ones we require
+                pageParts.hide().slice(start, end).show();
+            }
+        });
+        /*
+         This function Adds functionality to allow the traversing of the pagnination using the browsers forward/back functionality
+         Code was Implemented using
+         https://github.com/bilalakil/bin/blob/master/simplepagination/page-fragment/index.html
+         */
+        var checkFragment = function () {
+            var hash = window.location.hash || "#page-1";
+            // we'll use regex to check the hash string as follows:
+            // ^            strictly from the beginning of the string (i.e. succeed "#page-3" but fail "hi!#page-3")
+            // #page-       exactly match the text "#page-"
+            // (            start a matching group (so we can access what's in these parentheses on their own)
+            //      \d      any digit ([0-9])
+            //      +       one or more of the previous literal (one or more digits)
+            // )            end the matching group
+            // $            we should now be at the end of the string - if not, then don't match (i.e. fail "#page-3hi!")
+            hash = hash.match(/^#page-(\d+)$/);
+
+
+            if (hash) {
+                //A variable to store the page number
+                var hashInteger = parseInt(hash[1]);
+                //comparing if the link in url matches the pagination bounds of information stored, if so display data
+                //if not give feedback to user that there are no results on the current page.
+                if (hashInteger > numberOfPaginations) {
+                    $('#noResults').show();
+                    console.log("got in here??");
+                }
+                else {
+                    $('#noResults').hide();
+                }
+                // the selectPage function is one of many described in the documentation
+                // we've captured the page number in a regex group: (\d+)
+                console.log(parseInt(hash[1]));
+                $(".paginationHolder").pagination("selectPage", parseInt(hash[1]));
+            }
 
         }
-    });
-
-    /*
-     This function Adds functionality to allow the traversing of the pagnination using the browsers forward/back functionality
-     Code was Implemented using
-     https://github.com/bilalakil/bin/blob/master/simplepagination/page-fragment/index.html
-     */
-    var checkFragment = function () {
-        var hash = window.location.hash || "#page-1";
-        // we'll use regex to check the hash string as follows:
-        // ^            strictly from the beginning of the string (i.e. succeed "#page-3" but fail "hi!#page-3")
-        // #page-       exactly match the text "#page-"
-        // (            start a matching group (so we can access what's in these parentheses on their own)
-        //      \d      any digit ([0-9])
-        //      +       one or more of the previous literal (one or more digits)
-        // )            end the matching group
-        // $            we should now be at the end of the string - if not, then don't match (i.e. fail "#page-3hi!")
-        hash = hash.match(/^#page-(\d+)$/);
-
-
-        if (hash) {
-            //A variable to store the page number
-            var hashInteger = parseInt(hash[1]);
-            //comparing if the link in url matches the pagination bounds of information stored, if so display data
-            //if not give feedback to user that there are no results on the current page.
-            if (hashInteger > numberOfPaginations) {
-                $('#noResults').show();
-                //console.log("got in here??");
-            }
-            else {
-                $('#noResults').hide();
-            }
-            // the selectPage function is one of many described in the documentation
-            // we've captured the page number in a regex group: (\d+)
-            console.log(parseInt(hash[1]));
-            $(".paginationHolder").pagination("selectPage", parseInt(hash[1]));
-        }
+        $(window).bind("popstate", checkFragment);
 
     }
-    $(window).bind("popstate", checkFragment);
 
-    checkFragment();
-
-
-    //Search Functionality.
+    /*Search Functionality.
+    Search functionality implemented using example grabbed from
+    http://stackoverflow.com/questions/14031369/how-to-implement-search-function-using-javascript-or-jquery
+     */
     $("#searchFilter").on("keyup", function () {
         console.log("got in search function");
         var g = $(this).val().toLowerCase();
+        //iterate through all items containing class student-details, grabbing h3 to grab text of this variable.
         $(".student-details h3").each(function () {
             var s = $(this).text().toLowerCase();
+            //if the search finds a matching string entered, then
             if (s.indexOf(g) != -1) {
-                $(this).parent().parent().show();
+                //Show the item and add a class of searchSelected on its parents parent, ie the list item
+                $(this).parent().parent().show().addClass('searchSelected');
             }
             else {
-                $(this).parent().parent().hide();
+                //hide the item and remove the class if it exists of the list item.
+                $(this).parent().parent().hide().removeClass('searchSelected');
             }
+
         });
+        //Reset variable now using only elements containing only class of searchSelected
+        $pageParts = $(".searchSelected");
+        //recall the paginate functionality.
+        paginate($pageParts);
     });
 
+    //Invoking the pagination functionality when page loads
+    paginate($pageParts);
 
 });
